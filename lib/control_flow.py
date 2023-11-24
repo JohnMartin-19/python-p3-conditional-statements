0670a409599063cab156c3275717e3498cf1dffc
John <johnnkonge2020@gmail.com>
2023-11-24 10:03:59 +0300

    done

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,43 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    access='granted' if username == 'admin' & password == 12345 else 'denied'
+    print(access)
+admin_login('admin',12451)
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature <= 40 :
+        print('It is cold')
+    elif temperature > 40 < 65:
+        print('Too damn hot bro')
+    else: 
+        print('Perfect')
+hows_the_weather(25)
+
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num % 3 == 0:
+        print('Fizz')
+    elif num % 5 ==0:
+        print('Buzz')
+    else:
+        print(num)
+
+fizzbuzz(15)
 
 def calculator(operation, num1, num2):
     # your code here
-    pass
+    if operation == "+" :
+         print(num1 + num2)
+    elif operation == '-' :
+        print(num1 - num2)
+    elif operation == "*":
+        print(num1 * num2)
+    elif operation == "/":
+        print(num1/num2)
+    else:
+        print('Invalid Operation')
+    
+    calculator('+',10,5)
